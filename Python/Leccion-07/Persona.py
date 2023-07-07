66452f4441fc0c4a2160004829fbce0c73020646
Franco Bizzarri <105225095+franbizzarri@users.noreply.github.com>
2023-07-07 12:29:16 -0300

    UPDATE Curso de Python

--- Python/Leccion-07/Persona.py
+++ Python/Leccion-07/Persona.py
@@ -42,26 +42,60 @@ class Persona:
     mi_atributo = ''
 
     def __init__(self, nombre, apellido, edad):
-      self.nombre = nombre
-      self.apellido = apellido
-      self.edad = edad
+      self._nombre = nombre
+      self._apellido = apellido
+      self._edad = edad
     
     def mostrarDetalle(self):
-        print(f'Persona -> Nombre y apellido: {self.nombre} {self.apellido}, Edad: {self.edad}')
+      print(f'Persona -> Nombre y apellido: {self.nombre} {self.apellido}, Edad: {self.edad}')
+    
+    @property
+    def nombre(self):
+      return self._nombre
+    
+    @nombre.setter
+    def nombre(self, nombre):
+      self._nombre = nombre
+
+    @property
+    def apellido(self):
+      return self._apellido
+    
+    @apellido.setter
+    def apellido(self, apellido):
+      self._apellido = apellido
+    
+    @property
+    def edad(self):
+      return self._edad
+    
+    @edad.setter
+    def edad(self, edad):
+      self._edad = edad
 
 
 persona1 = Persona('Franco', 'Bizzarri', 30)
 persona2 = Persona('Sofia', 'Perviu', 28)
 persona3 = Persona('Emma', 'Bizzarri', 2)
 
-persona1.mostrarDetalle()
-persona2.mostrarDetalle()
-persona3.mostrarDetalle()
 
-Persona.mostrarDetalle(persona1) # Otra forma de ejecutar un metodo de una clase aunque no es lo comun
+# persona1.mostrarDetalle()
+# persona2.mostrarDetalle()
+# persona3.mostrarDetalle()
+
+# Persona.mostrarDetalle(persona1) # Otra forma de ejecutar un metodo de una clase aunque no es lo comun
 
 # CREAR UN NUEVO ATRIBUTO A UN OBJETO
 
-persona1.telefono = '2616525783'
+# persona1.telefono = '2616525783'
 
-print(f'Telefono - Persona 1: {persona1.telefono} ')
+# print(f'Telefono - Persona 1: {persona1.telefono} ')
+
+# METODO GET()
+
+print(f'Nombre: {persona1.nombre}')
+
+# METODO SET()
+
+persona1.nombre = 'Maximiliano'
+print(f'Nombre modificado: {persona1.nombre}')
---
+++ Python/Leccion-07/PruebaPersona.py
@@ -0,0 +1,4 @@
+from Persona import Persona     # Para importar todas las clases de un archivo se coloca asterisco(*) Ej: from Persona import *
+
+persona1 = Persona('Ivo', 'Bizzarri', 70)
+persona1.mostrarDetalle()
